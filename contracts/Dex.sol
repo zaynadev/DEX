c786a9c399187271b3041a41d6f2fb777ff67a50
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-07-11 15:42:36 +0100

    add deposit and withdraw function

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
 
     struct Token {
-        bytes32 tiker;
+        bytes32 ticker;
         address tokenAddress;
     }
     mapping(bytes32 => Token) public tokens;
+    mapping(address => mapping(bytes32 => uint)) public balance;
     bytes32[] public tokenList;
     address public admin;
 
@@ -20,9 +22,26 @@ contract DEX {
         _;
     }
 
-    function addToken(bytes32 _tiker, address _tokenAddress) external {
-        tokens[_tiker] = Token(_tiker, _tokenAddress);
-        tokenList.push(_tiker);
+    modifier tickerExist(bytes32 ticker){
+        require(tokens[ticker].tokenAddress != address(0), "token doesnt exist");
+        _;
+    }
+
+
+    function addToken(bytes32 _ticker, address _tokenAddress) external {
+        tokens[_ticker] = Token(_ticker, _tokenAddress);
+        tokenList.push(_ticker);
+    }
+
+    function deposit(bytes32 ticker, uint amount) tickerExist(ticker) external {
+        IERC20(tokens[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        balance[msg.sender][ticker] += amount; 
+    }
+
+    function withdraw(bytes32 ticker, uint amount) tickerExist(ticker) external {
+        require(balance[msg.sender][ticker] >= amount, "balance too low");
+        balance[msg.sender][ticker] -= amount;
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
     }
 
 }
